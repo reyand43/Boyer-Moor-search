5154ca5d819166abbbccbfa40d3a532afe51151d
Andrey Babushkin <55714097+whitespirit63@users.noreply.github.com>
2020-12-05 20:38:33 +0300

    Add files via upload

--- sol.py
+++ sol.py
@@ -3,6 +3,8 @@ import random
 import string
 from termcolor import colored, cprint
 
+result = []
+text = []
 def forming_d(pattern):
     """ Формируем массив d."""
     d = [len(pattern) for i in range(256)] #заполняем массив числами длины паттерна
@@ -26,19 +28,22 @@ def search(string, pattern):
     # k - проход по string
     # s - количество совпавших символов
     len_p = x = j = k = len(pattern)
-    # число смещений
     s = 0
     counter = 0
+    temp = 0
     while x<=len(string) and s<(len(pattern)):
+        temp +=1
         if pattern[j - 1] == string[k - 1]:
             ''' РАСКОММЕНТИРОВАТЬ, ЕСЛИ НЕОБХОДИМО ПОСМОТРЕТЬ СРАВНЕНИЕ СЛОВ'''
             #print(string[k - 1], '==', pattern[j - 1])
             j -= 1
             k -= 1
             s +=1
+            
         else: 
             ''' РАСКОММЕНТИРОВАТЬ, ЕСЛИ НЕОБХОДИМО ПОСМОТРЕТЬ СРАВНЕНИЕ СЛОВ'''
             #print(string[k - 1], '!=', pattern[j - 1])
+           
             d1=max(d[ord(string[k - 1])]-s, 1)
             d2=len(pattern)-1-pattern.find(string[x - 1])
             if (s == 0):
@@ -67,31 +72,40 @@ def search(string, pattern):
             print(string[i], end='')
         
     print('')
+    result.append(temp)
     if s == len(pattern):
-        return "Нашли. Число смещений равно %d." % counter
+        return "Нашли. Число сравнений равно %d." % temp
     else:
         return "Не нашли!"
  
 
+if __name__ == "__main__":
+    words = "./dict.txt"
+    WORDS = open(words).read().splitlines()
+    interval = 100
+    for i in range(20):
+        arr = np.array([random.randint(0, 25486) for i in range(random.randint(interval, interval))])
+        x = 0
+        string = ''
+        stringArr = []
+        for a in arr:
+        
+            string =  WORDS[a] + ' ' + string
+            stringArr.append(WORDS[a])
+            x += 1
 
-arr = np.array([random.randint(0, 25487) for i in range(random.randint(50, 100))])
-words = "./dict.txt"
-WORDS = open(words).read().splitlines()
-
-x = 0
-string = ''
-stringArr = []
-for a in arr:
-    string =  WORDS[a] + ' ' + string
-    stringArr.append(WORDS[a])
-    x += 1
-
-print('ИССЛЕДУЕМАЯ СТРОКА:', string)
-patternNumber = random.randint(0, len(stringArr)-1)
-pattern = stringArr[patternNumber]
-print('ИСКОМАЯ ПОДСТРОКА:', pattern)
-print('')
-print(search(string, pattern))
-
-
+        # print('ИССЛЕДУЕМАЯ СТРОКА:', string)
+        # patternNumber = random.randint(0, len(stringArr)-1)
+        patternNumber = 0
+        pattern = stringArr[patternNumber]
+        #print('Длина строки', len(string))
+        text.append(len(string))
+        print('')
+        print('ИСКОМАЯ ПОДСТРОКА:', pattern)
+        
+        print(search(string, pattern))
+    print('')
+    print('средняя длина текста равна', np.mean(text))
+    print('средняя количество сравнений равно',np.mean(result))
+    
 
